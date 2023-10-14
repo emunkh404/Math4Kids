96370e81a0b2238b35796608ae15bb0bab716b70
Munkh Erdene <m.erdene404@gmail.com>
2023-10-14 18:48:22 -0400

    func update

--- cloud-functions/secret-page.js
+++ cloud-functions/secret-page.js
@@ -1,6 +1,24 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: `Welcome to "Users page" !!!`,
-  });
+  const secretContent = `
+        <h3> Welcome to Secret Page</h3>
+        <p>this is for registered <strong>user</strong> only page</p>
+    `;
+
+  let body;
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if (body.password == "MyPassword") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 404,
+    });
+  }
 };
